841d3d971342b7ad164960b26b4d559b7c1eb80c
Matija Miletic <matijamiletic@protonmail.com>
2023-07-10 20:34:32 +1200

    Completed animation exercise

--- exercises/import-from-web/main.js
+++ exercises/import-from-web/main.js
@@ -1,12 +1,9 @@
 import { motion } from 'https://cdn.skypack.dev/motion'
 
-// Create a new motion instance
 const motionInstance = new motion()
 
-// Get the body element
 const body = document.querySelector('body')
 
-// Demo animation
 motionInstance.animate(
   body,
   { translateX: '100px' },
